5d14332d803b7567e51fb8f9d4f6ee6c67541b65
kk995 <57400216+kk995@users.noreply.github.com>
2019-11-27 20:58:49 +0800

    实验二
    实验二代码

---
+++ SubstractOperation.java
@@ -0,0 +1,11 @@
+public class SubstractOperation extends BinaryOperation_3_2 {	
+	public SubstractOperation() {	
+		generateBinaryOperation('-');		
+	}	
+	boolean checkingCalculation(int anInteger){
+		return anInteger >= LOWER;
+	}
+	int calculate(int left, int right){
+		return left-right;
+	}		
+}
